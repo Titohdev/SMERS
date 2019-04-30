78aafe80aa2491e4d9c5b83246a54c7cb00cfc0d
owoseni timileyin <owoseniboluwatife70@gmail.com>
2019-04-30 15:40:50 +0100

    updates

--- app/src/main/java/com/example/smers/Register.java
+++ app/src/main/java/com/example/smers/Register.java
@@ -1,13 +1,25 @@
 package com.example.smers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
-
+Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        b = findViewById(R.id.register);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Register.this,Login.class));
+            }
+        });
+
+       // startActivity(new Intent(Register.this,Login.class));
     }
 }
--- app/src/main/java/com/example/smers/Splash.java
+++ app/src/main/java/com/example/smers/Splash.java
@@ -1,12 +1,13 @@
 package com.example.smers;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
 
-    private static int timer=3000;
+    private static int timer= 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +17,11 @@ public class Splash extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-
+            startActivity(new Intent(Splash.this,Register.class));
+                finish();
             }
-        }, timer);
+
+        }, timer *1000 );
     }
+
 }
